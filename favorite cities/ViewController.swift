78d0de47deb555beb92c325521fa124494ea9336
Joey Young <josephyoung1@yahoo.com>
2020-01-10 10:22:55 -0600

    Initial Commit

---
+++ favorite cities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite cities
+//
+//  Created by joey young on 1/10/20.
+//  Copyright © 2020 joey young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
